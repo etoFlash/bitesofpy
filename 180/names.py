39aecee7cbe3bf8f4f50893b953f5dd74a62d30b
etoFlash <>
2019-10-30 20:38:30 +0300

    add bite 180

---
+++ 180/names.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+# fake data from https://www.mockaroo.com
+data = """last_name,first_name,country_code
+Watsham,Husain,ID
+Harrold,Alphonso,BR
+Apdell,Margo,CN
+Tomblings,Deerdre,RU
+Wasielewski,Sula,ID
+Jeffry,Rudolph,TD
+Brenston,Luke,SE
+Parrett,Ines,CN
+Braunle,Kermit,PL
+Halbard,Davie,CN"""
+
+
+def group_names_by_country(data: str = data) -> defaultdict:
+    countries = defaultdict(list)
+    for i, line in enumerate(data.splitlines()):
+        if not i:
+            continue
+        name, surname, country = line.split(",")
+        countries[country].append(f"{surname} {name}")
+
+    return countries
