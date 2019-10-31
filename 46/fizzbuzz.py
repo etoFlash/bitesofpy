edc6127848bb3a8ec51c56fbbbe0da7e1591fc52
etoFlash <flashback.rce@gmail.com>
2019-10-31 18:13:20 +0300

    add bite 46

--- 46/fizzbuzz.py
+++ 46/fizzbuzz.py
@@ -1,2 +1,7 @@
 def fizzbuzz(num):
-    pass
+    result = ""
+    if not num % 3:
+        result = "Fizz"
+    if not num % 5:
+        result = "Buzz" if not result else result + " Buzz"
+    return num if not result else result
