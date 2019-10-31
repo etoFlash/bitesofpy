730e43da88aadc458a6c984b9152764f1647e301
etoFlash <flashback.rce@gmail.com>
2019-10-31 19:36:24 +0300

    add_bites.py: refactor, add regex patterns on future

--- add_bites.py
+++ add_bites.py
@@ -1,7 +1,5 @@
 # author: @Flash (https://github.com/etoFlash)
-# unzip archives with names like pybites_bite[%NUMBER%].zip to folders with names like [%NUMBER%]
-# for supporting submit solution to https://codechalleng.es/bites/ through github repository
-# TODO: 1) extract title, level and tags by requests by regex from codechalleng.es (exaple:
+# TODO: 1) extract title, level and tags by requests by regex from codechalleng.es (example:
 #  re.search(r"Bite ([0-9]{1-3}.*?)</title>", str(r.content))
 # TODO: 2) add info about bite to solved_bites.md
 # TODO: 3) add to top in py-file in new folder comment "# TODO: beat it" for made more easy search not solved bites
@@ -11,13 +9,21 @@ import os
 import zipfile
 files_to_unzip = []
 files_unzipped = []
+unzip_errors = []
+num = 0
+bite_page = f"https://codechalleng.es/bites/{num}"
+filename_pattern = r".*?([0-9]{1,3})\.zip"
+title_pattern = r"Bite ([0-9]{1-3}.*?)</title>"
+tag_pattern = r"<a class=\"tag\" href=\"(.*?)\">(.*?)</a>"
+level_pattern = r"<img class=\"biteImg\" src=\".*?\" alt=\"(.*?) level\">"
+
 for file in glob.glob("*.zip"):
-    print(file)
-    result = re.search(r".*?([0-9]{1,3})\.zip", file)
+    result = re.search(filename_pattern, file)
     if result:
         files_to_unzip.append(file)
         num = result.group(1)
         if os.path.exists(num):
+            unzip_errors.append("folder exists")
             continue
         with zipfile.ZipFile(file) as zip_file:
             zip_file.extractall(num)
@@ -26,6 +32,12 @@ for file in glob.glob("*.zip"):
         os.remove(file)
 
 if files_unzipped:
-    print("Unzipped:\n", files_unzipped)
+    print("Unzipped:")
+    for file in files_unzipped:
+        print(f" {file}")
 if files_to_unzip:
-    print("Not unzipped, because folders exists:\n", files_to_unzip)
+    print("Not unzipped (file - reason):")
+    for file, reason in zip(files_to_unzip, unzip_errors):
+        print(f" {file} - {reason}")
+if not (files_unzipped or files_to_unzip):
+    print("Nothing to process")
