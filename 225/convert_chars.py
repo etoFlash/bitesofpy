c8befca86b1964000def9310f4cd6f51ddfda33c
etoFlash <flashback.rce@gmail.com>
2019-11-06 03:42:42 +0300

    Refactor Bite 225

--- 225/convert_chars.py
+++ 225/convert_chars.py
@@ -4,4 +4,5 @@ PYBITES = "pybites"
 def convert_pybites_chars(text):
     """Swap case all characters in the word pybites for the given text.
        Return the resulting string."""
-    return "".join(c.swapcase() if c.lower() in PYBITES else c for c in text)
+    return "".join(c.swapcase() if c.lower() in PYBITES else c
+                   for c in text)
