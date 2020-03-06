e902b188901e8589caed11006fb282fe735a637d
etoFlash <flashback.rce@gmail.com>
2020-03-06 04:11:07 +0300

    Fix Bite 47

--- 47/password.py
+++ 47/password.py
@@ -7,11 +7,11 @@ used_passwords = set('PassWord@1 PyBit$s9'.split())
 
 
 def validate_password(password):
-    if 6 <= len(password) <= 12 and\
-      re.search(r"[0-9]", password) and\
-      len(re.findall(r"[a-z]", password)) > 1 and\
-      re.search(r"[A-Z]", password) and\
-      re.search(rf"[{string.punctuation}]", password) and\
+    if 6 <= len(password) <= 12 and \
+      re.search(r"[0-9]", password) and \
+      len(re.findall(r"[a-z]", password)) > 1 and \
+      re.search(r"[A-Z]", password) and \
+      re.search(rf"[{string.punctuation}]", password) and \
       password not in used_passwords:
         used_passwords.add(password)
         return True
