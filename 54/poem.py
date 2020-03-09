4d44e1085d14d360a728e3a7007fae0e9399f60a
etoFlash <flashback.rce@gmail.com>
2020-03-09 03:27:38 +0300

    Refactor Bite 54

--- 54/poem.py
+++ 54/poem.py
@@ -7,8 +7,4 @@ INDENTS = 4
 def print_hanging_indents(poem):
     for part_poem in poem.split("\n\n"):
         for (i, s) in enumerate(part_poem.split("\n")):
-            line = s.strip()
-            if not i:
-                print(line)
-            else:
-                print(indent(line, " " * INDENTS))
+            print(indent(s.strip(), " " * INDENTS, lambda _: i > 0))
