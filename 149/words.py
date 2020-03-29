4ed7250b75b0fbc6d174e39d7184dadf28fa5a6d
etoFlash <flashback.rce@gmail.com>
2020-03-29 23:55:44 +0300

    Add Bite 149

---
+++ 149/words.py
@@ -0,0 +1,7 @@
+def sort_words_case_insensitively(words):
+    """Sort the provided word list ignoring case, and numbers last
+       (1995, 19ab = numbers / Happy, happy4you = strings, hence for
+        numbers you only need to check the first char of the word)
+    """
+    return sorted(words,
+                  key=lambda x: (1 if x[0].isdigit() else 0, x.lower()))
