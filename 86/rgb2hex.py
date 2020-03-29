1b9cf14b2e836bc2c31f11eedd4eaeacdf58ce0d
etoFlash <flashback.rce@gmail.com>
2020-03-30 01:11:08 +0300

    Refactor Bite 86

--- 86/rgb2hex.py
+++ 86/rgb2hex.py
@@ -4,4 +4,4 @@ def rgb_to_hex(rgb):
        Silver: input tuple = (192,192,192) -> output hex str = #C0C0C0"""
     if not all(255 >= n >= 0 for n in rgb) or len(rgb) != 3:
         raise ValueError
-    return "#" + "".join(format(n, 'x').zfill(2) for n in rgb).upper()
+    return "#" + "".join(f"{n:02x}" for n in rgb).upper()
