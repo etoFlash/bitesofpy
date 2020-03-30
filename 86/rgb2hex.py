c7df1741d82f2a67f8e81de02c39846c88a1990d
etoFlash <flashback.rce@gmail.com>
2020-03-30 03:41:20 +0300

    Refatcor Bite 86

--- 86/rgb2hex.py
+++ 86/rgb2hex.py
@@ -2,6 +2,6 @@ def rgb_to_hex(rgb):
     """Receives (r, g, b)  tuple, checks if each rgb int is within RGB
        boundaries (0, 255) and returns its converted hex, for example:
        Silver: input tuple = (192,192,192) -> output hex str = #C0C0C0"""
-    if not all(255 >= n >= 0 for n in rgb) or len(rgb) != 3:
+    if not all(0 <= n <= 255 for n in rgb) or len(rgb) != 3:
         raise ValueError
     return "#" + "".join(f"{n:02x}" for n in rgb).upper()
