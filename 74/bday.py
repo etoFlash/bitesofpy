87e43023daca26439ca9035c7c61f2102919cdc7
etoFlash <flashback.rce@gmail.com>
2020-04-10 23:57:55 +0300

    Refactor Bite 74

--- 74/bday.py
+++ 74/bday.py
@@ -3,5 +3,5 @@ import calendar
 
 def weekday_of_birth_date(date):
     """Takes a date object and returns the corresponding weekday string"""
-    day = calendar.weekday(year=date.year, month=date.month, day=date.day)
+    day = date.weekday()
     return calendar.day_name[day]
