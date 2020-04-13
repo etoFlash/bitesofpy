cfebb248d47379f8389970e06850632191942585
etoFlash <flashback.rce@gmail.com>
2020-04-13 23:58:02 +0300

    Refactor Bite 163

--- 163/reqs.py
+++ 163/reqs.py
@@ -1,11 +1,10 @@
+from distutils.version import StrictVersion
+
+
 def _extract_req_dict(reqs: str) -> dict:
    return dict(
       s.split("==") for s in reqs.strip().splitlines(keepends=False)
    )
-
-
-def _extract_version_tuple(version: str) -> tuple:
-   return tuple(map(int, version.split(".")))
 
 
 def changed_dependencies(old_reqs: str, new_reqs: str) -> list:
@@ -13,12 +12,8 @@ def changed_dependencies(old_reqs: str, new_reqs: str) -> list:
        and return a list of dependencies that have been upgraded
        (have a newer version)
     """
-    result = []
-    old_reqs_dict = _extract_req_dict(old_reqs)
-    new_reqs_dict = _extract_req_dict(new_reqs)
-    for k, v in old_reqs_dict.items():
-       if _extract_version_tuple(new_reqs_dict[k]) > \
-           _extract_version_tuple(v):
-          result.append(k)
-
-    return result
+    old = _extract_req_dict(old_reqs)
+    new = _extract_req_dict(new_reqs)
+    for k, v in old.items():
+       if StrictVersion(new[k]) > StrictVersion(v):
+          yield k
