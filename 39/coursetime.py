20602ad4a478eee0e65404f0d89ebf36c20353ff
etoFlash <flashback.rce@gmail.com>
2020-04-19 00:16:16 +0300

    Refactor Bite 39

--- 39/coursetime.py
+++ 39/coursetime.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 import os
 import re
 import urllib.request
@@ -25,21 +25,19 @@ def get_all_timestamps():
 
         Return a list of MM:SS timestamps
     """
-    timestamps = []
     with open(COURSE_TIMES) as f:
-        for line in f:
-            r = re.search(r"\((\d{1,2}:\d{1,2})\)", line)
-            if r:
-                timestamps.append(r.group(1))
+        data = f.read()
+        r = re.findall(r"\((\d{1,2}:\d{1,2})\)", data)
 
-    return timestamps
+    return r
 
 
 def calc_total_course_duration(timestamps):
     """Takes timestamps list as returned by get_all_timestamps
        and calculates the total duration as HH:MM:SS"""
-    d = datetime(1970, 1, 1)
+    td = timedelta()
     for timestamp in timestamps:
-        t = timestamp.split(":")
-        d += timedelta(minutes=int(t[0]), seconds=int(t[1]))
-    return d.strftime("%H:%M:%S")
+        minutes, seconds = timestamp.split(":")
+        td += timedelta(minutes=int(minutes), seconds=int(seconds))
+
+    return str(td)
