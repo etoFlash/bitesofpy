2990b652b762539e7f43ba2815d49275f7d1aa9f
etoFlash <flashback.rce@gmail.com>
2020-08-23 05:40:27 +0300

    Refactor Bite 161

--- 161/tree.py
+++ 161/tree.py
@@ -1,18 +1,20 @@
 import os
-import glob
+from functools import reduce
 
 
 def count_dirs_and_files(directory='.'):
     """Count the amount of of directories and files in passed in "directory" arg.
        Return a tuple of (number_of_directories, number_of_files)
     """
-    dirs_count = 0
-    files_count = 0
+    dirs, files = 0, 0
 
-    for dir_or_file in glob.glob(os.path.join(directory, "**"), recursive=True)[1:]:
-        if os.path.isdir(dir_or_file):
-            dirs_count += 1
-        elif os.path.isfile(dir_or_file):
-            files_count += 1
+    for _, dirs_list, files_list in os.walk(directory):
+        dirs += len(dirs_list)
+        files += len(files_list)
 
-    return dirs_count, files_count
+    return dirs, files
+
+    # functional variant:
+    # return reduce(lambda a, b: (a[0] + b[0], a[1] + b[1]), (
+    #     (len(dirs), len(files)) for _, dirs, files in os.walk(directory)
+    # ))
