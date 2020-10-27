d69377f6b5941b3517ce2ec5cb525a1466755bad
etoFlash <flashback.rce@gmail.com>
2020-10-28 00:16:56 +0300

    Refactor Bite 155

--- 155/split.py
+++ 155/split.py
@@ -1,3 +1,6 @@
+from shlex import split
+
+
 def split_words_and_quoted_text(text: str):
     """Split string text by space unless it is
        wrapped inside double quotes, returning a list
@@ -10,15 +13,4 @@ def split_words_and_quoted_text(text: str):
        the resulting list would be:
        ['Should', 'give', '3 elements only']
     """
-    result = []
-    while text:
-        if text.startswith('"'):
-            _, s, text = text.split('"', 2)
-            text = text.lstrip(' ')
-        elif not text.count(' '):
-            s, text = text, None
-        else:
-            s, text = text.split(' ', 1)
-        result.append(s)
-
-    return result
+    return split(text)
